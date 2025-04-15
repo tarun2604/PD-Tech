55fc949abdb54d792a4c48e8106cb6743dd59dbe
tarun2604 <tarunkothamasu2604@gmail.com>
2025-04-15 05:37:39 +0530

    sixth commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6c6c3b19'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rqi2d9uid6g"
+    "revision": "0.bp9p9mkltmg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
